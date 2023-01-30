df75dcdc066180b3f7eaef86aea49194e95e24ab
Eduard Kelmann <virusscanner06@web.de>
2023-01-30 23:13:52 +0100

    Update text_to_speech.py

--- src/text_to_speech.py
+++ src/text_to_speech.py
@@ -7,6 +7,8 @@ def text_to_speech(text) -> None:
     config.read('config/config.ini')
     engine = pyttsx3.init()
     engine.setProperty('rate', config['SPEECH']['speed'])
+    engine.setProperty('voice', 'german')
+
     engine.say(text)
     engine.runAndWait()
 
